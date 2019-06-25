4ecfbdce26b6ccb9649a03b784ff543f1d13486b
ko-he- <koheiyatsushiro@yahoo.co.jp>
2019-06-25 02:13:46 +0000

    Updates

---
+++ precache-manifest.77359528aee869e9daf09bc26e6bc440.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1730fe42cd36d33690cb9ee246882e1",
+    "url": "/scrum-burndown-chart/index.html"
+  },
+  {
+    "revision": "f3de8d5c2be49a6f93ad",
+    "url": "/scrum-burndown-chart/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "fb41e0a320f1c680e072",
+    "url": "/scrum-burndown-chart/static/js/2.ac2f4746.chunk.js"
+  },
+  {
+    "revision": "f3de8d5c2be49a6f93ad",
+    "url": "/scrum-burndown-chart/static/js/main.55e9f57d.chunk.js"
+  },
+  {
+    "revision": "5346f0ff2974eff92b2f",
+    "url": "/scrum-burndown-chart/static/js/runtime~main.16402a65.js"
+  }
+]);
